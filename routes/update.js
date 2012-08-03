37f02ae5928f175828fa445cc0be1cff0e0a578d
Michael Nisi <michael.nisi@gmail.com>
2012-08-03 19:05:27 +0200

    Do not change process dir in update

--- routes/update.js
+++ routes/update.js
@@ -6,7 +6,6 @@ var blake = require('blake')
 function update (req, res) {
   res.writeHead(200) 
   res.end()
-  process.chdir('/')
   blake(c.source, c.target, c.tweet, c.likes, function (err) {
     console.log('%s: tweet and likes updated', new Date())  
   })
