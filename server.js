a50ddb6030561c15cc32e3d5631954c6a27494c6
Michael Nisi <michael.nisi@gmail.com>
2012-11-03 17:57:45 +0100

    Add push route

--- server.js
+++ server.js
@@ -1,16 +1,53 @@
-module.exports = server
+var config = require('./config.js')
+, worker = require.resolve('./worker.js')
+, cluster = require('cluster')
+, clusterConf = config.cluster || {}
+clusterConf.exec = worker
 
-var http = require('http')
-  , port = process.argv.splice(2)[0]
+process.stdout.on('error', function (er) {
+  if (er.code === 'EPIPE') return
+  throw er
+})
 
-if (!port) {
-  throw new Error('No port provided.')
-}
+// set up the server cluster.
+var clusterMaster = require("cluster-master")
+, npm = require("npm")
 
-function server (route) {
-  var me = http.createServer(route).listen(port)
-  
-  me.port = port
+config.log.master = true
+var logger = require('bunyan').createLogger(config.log)
 
-  return me
+console.error = logger.warn.bind(logger)
+console.log = logger.info.bind(logger)
+
+var npmconf = config.npm || {}
+npmconf["node-version"] = null
+npm.load(npmconf, function (er) {
+  if (er) throw er
+
+  // Ok, we're ready!  Spin up the cluster.
+  clusterMaster(clusterConf)
+})
+
+// if there's an http port, then redirect from there to https.
+// this almost never needs to change, so just run it in the master,
+// rather than having each worker to it.
+if (config.https && config.httpPort) {
+  var h = 'https://' + config.host
+  if (config.port && config.port !== 443) h += ':' + config.port
+  var canon = config.canon = require('canonical-host')(h, 301)
+  var http = require('http')
+  httpServer = http.createServer(function (req, res) {
+    if (canon(req, res)) return
+    // wtf?
+    res.statusCode = 400
+    res.end('bad')
+  })
+  httpServer.listen(config.httpPort)
+  // restart on crashes
+  httpServer.on('close', function () {
+    console.error('http->https redirector crashed')
+    setTimeout(function () {
+      httpServer.listen(config.httpPort)
+    }, 100)
+  })
 }
