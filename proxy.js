0637ccd41335e42cbf99551fd46d6f1697514aae
Michael Nisi <michael.nisi@gmail.com>
2012-11-04 10:54:54 +0100

    Write proxy

--- proxy.js
+++ proxy.js
@@ -1,12 +1,24 @@
-var config = require('./config') 
-  , bouncy = require('bouncy')
+var bouncy = require('bouncy')
+  , http = require('http')
+  , url = require('url')
+  , path = require('path')
+  , config = require('./config.js')
   , ports = config.ports
-  , port
-  , i = 0
 
 bouncy(function (req, bounce) {
-  port = ports[i++]
-  bounce(port)
-  if (i >= ports.length) i = 0
+  var parsed = url.parse(req.url)
+    , pathname = parsed.pathname
+    , normalPathname = path.normalize(pathname).replace(/\\/g, '/')
+    , port = ports[normalPathname]
+  
+  if (port) {
+    bounce(port)
+  } else {
+    var res = bounce.respond()
+      , code = http.STATUS_CODES[404]
+    
+    res.writeHead(code)
+    res.end(code + '\n')
+  }
 }).listen(config.port)
 
