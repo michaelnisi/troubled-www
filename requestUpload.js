905cfa8e0b51305692680f1f4c7aac8e0626c757
Michael Nisi <michael.nisi@gmail.com>
2012-11-12 21:08:34 +0100

    Upload after update

---
+++ requestUpload.js
@@ -0,0 +1,17 @@
+
+// requestUpload - request upload to S3
+
+var url = require('url')
+  , querystring = require('querystring')
+  , request = require('request')
+  , config = require('../config.js)
+
+module.exports = function (message) {
+  var upload = config.urls.upload
+
+  upload.query = querystring.stringify({ message: message })
+
+  var uri = url.format(upload)
+
+  return request.post(uri)
+}
--- routes/update.js
+++ routes/update.js
@@ -5,6 +5,7 @@ var blake = require('blake')
   , cop = require('cop')
   , config = require('../config.js')
   , readArray = require('event-stream').readArray
+  , requestUpload = require('../lib/requestUpload.js')
   , filenames = [config.likes, config.tweet]
   , source = config.source
   , target = config.target
@@ -12,6 +13,9 @@ var blake = require('blake')
 module.exports = function (req, res) {
   readArray(filenames)
     .pipe(blake(source, target))
-    .pipe(cop(function (filename) { return filename + '\n' }))
-    .pipe(res)
+    .on('end', function () {
+      requestUpload('Update tweet and likes')
+        .on('error', console.error)
+        .pipe(process.stdout)
+    })
 }
