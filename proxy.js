1415cec4054d069be3ae7eda7ae619c98aa4d2ab
Michael Nisi <michael.nisi@gmail.com>
2012-11-20 17:51:12 +0100

    Move normalize

--- proxy.js
+++ proxy.js
@@ -3,22 +3,23 @@
 
 var http = require('http')
   , url = require('url')
-  , path = require('path')
+  , normalize = require('lib/normalize.js')
   , bouncy = require('bouncy')
   , config = require('./config.js')
 
 bouncy(function (req, bounce) {
   var parsed = url.parse(req.url)
     , pathname = parsed.pathname
-    , normalPathname = path.normalize(pathname).replace(/\\/g, '/')
+    , normalPathname = normalize(pathname)
     , id = normalPathname.replace(/\//g, '')
     , uri = config.urls[id]
-    , port = (uri && uri.open) ? uri.port : null
+    , port = uri && uri.open ? uri.port : null
     , err = port ? null : new Error('Unknown URL')
 
   function end () {
     var code = http.STATUS_CODES[404]
       , res = bounce.respond()
+
     res.writeHead(code)
     res.end(code + '\n')
   }
