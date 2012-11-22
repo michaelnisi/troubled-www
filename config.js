f09ca1f14092649bbd1a132a7650dab0a337f3b7
Michael Nisi <michael.nisi@gmail.com>
2012-11-22 10:59:35 +0100

    Unclutter

--- config.js
+++ config.js
@@ -9,18 +9,16 @@ var resolve = require('path').resolve
   , source = config.source
   , target = config.target
   , urls = Object.create(null)
-  , url = null
 
 exports.env = process.env.NODE_ENV
 exports.source = source
 exports.target = target
-exports.port = config.port || 8080
-exports.validate = config.validate !== undefined ? config.validate : true
 
-urls.publish = { port: 8081, open: true }
+urls.publish = { port: 8081 }
 urls.upload  = { port: 8082 }
 urls.update  = { port: 8083 }
 
+var url = null
 Object.keys(urls).forEach(function (key) {
   url = urls[key]
   url.protocol = 'http'
@@ -31,12 +29,6 @@ exports.urls = config.urls || urls
 exports.delay = config.delay || 3600000
 exports.tweet = resolve(source, 'data', 'tweet.json')
 exports.likes = resolve(source, 'data', 'likes.json')
-
-exports.github = [
-  '207.97.227.253'
-, '50.57.128.197'
-, '108.171.174.178'
-]
 
 if (module === require.main) {
   console.log(exports)
