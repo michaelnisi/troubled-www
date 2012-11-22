02d3f75f6e18c15452739a767b31f5b3f45939f4
Michael Nisi <michael.nisi@gmail.com>
2012-11-22 18:21:16 +0100

    Streamline

--- config.js
+++ config.js
@@ -6,21 +6,19 @@ var resolve = require('path').resolve
   , fs = require('fs')
   , source = process.env.TROUBLED_SOURCE
   , target = process.env.TROUBLED_TARGET
-  , urls = Object.create(null)
 
 fs.statSync(source)
 fs.statSync(target)
 
-urls.publish = url.parse('http://localhost:8081')
-urls.upload  = url.parse('http://localhost:8082')
-urls.update  = url.parse('http://localhost:8083')
-
-exports.urls = urls
 exports.source = source
 exports.target = target
 exports.delay = 3600000
 exports.tweet = resolve(source, 'data', 'tweet.json')
 exports.likes = resolve(source, 'data', 'likes.json')
+
+exports.publish = url.parse('http://localhost:8081')
+exports.upload  = url.parse('http://localhost:8082')
+exports.update  = url.parse('http://localhost:8083')
 
 if (module === require.main) {
   console.log(exports)
